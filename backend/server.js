1e891516511c7849b54addd229192fda73dccb33
okethan35 <ethantran35@hotmail.com>
2025-02-07 10:49:45 -0800

    Updated configs

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("MongoDB Connection Error: ", err));
 
