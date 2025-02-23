c9305b09f7a542ab26c55b24098c49282ad01899
dhruvpatel-05 <dhruvp2106@gmail.com>
2025-02-22 21:55:59 -0800

    Added username in signup and navbar to signup and login pages

--- boardify/src/components/Login.js
+++ boardify/src/components/Login.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Navbar from "./NavBar.js";
 import '../styles/Login.css';
 
 const Login = () => {
@@ -11,21 +12,24 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="input-group">
-            <label>Email</label>
-            <input type="email" required />
-          </div>
-          <div className="input-group">
-            <label>Password</label>
-            <input type="password" required />
-          </div>
-          <button type="submit" className="login-button">Login</button>
-        </form>
-        <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
+    <div>
+      <Navbar/>
+      <div className="login-container">
+        <div className="login-box">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label>Email</label>
+              <input type="email" placeholder="Email" required />
+            </div>
+            <div className="input-group">
+              <label>Password</label>
+              <input type="password" placeholder="Password"  required />
+            </div>
+            <button type="submit" className="login-button">Login</button>
+          </form>
+          <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
+        </div>
       </div>
     </div>
   );
--- boardify/src/components/SignUp.js
+++ boardify/src/components/SignUp.js
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import "../styles/Login.css";
+import Navbar from "./NavBar.js";
 
-const Signup = () => {
+const SignUp = () => {
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -10,23 +11,30 @@ const Signup = () => {
   };
 
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2>Signup</h2>
-        <form onSubmit={handleSubmit}>
+    <div>
+      <Navbar/>
+      <div className="login-container">
+        <div className="login-box">
+          <h2>Signup</h2>
+          <form onSubmit={handleSubmit}>
           <div className="input-group">
-            <label>Email</label>
-            <input type="email" placeholder="Email" required />
-          </div>
-          <div className="input-group">
-            <label>Password</label>
-            <input type="password" placeholder="Password" required />
-          </div>
-          <button type="submit" className="login-button">Signup</button>
-        </form>
+              <label>Username</label>
+              <input type="username" placeholder="Username" required />
+            </div>
+            <div className="input-group">
+              <label>Email</label>
+              <input type="email" placeholder="Email" required />
+            </div>
+            <div className="input-group">
+              <label>Password</label>
+              <input type="password" placeholder="Password" required />
+            </div>
+            <button type="submit" className="login-button">Signup</button>
+          </form>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Signup;
+export default SignUp;
