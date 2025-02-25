11db117c816a6ea09adcad71fd793c42bcbcf05c
okethan35 <ethantran35@hotmail.com>
2025-02-24 17:01:28 -0800

    Started setting up routing

--- boardify/src/App.jsx
+++ boardify/src/App.jsx
@@ -1,17 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Home from "./components/Home";
+import './App.css';
+import Home from 'Pages/Home'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
