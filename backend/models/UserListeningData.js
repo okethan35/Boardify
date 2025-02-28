273f0694b5c867a0d7e6323ec23efc49f987deda
okethan35 <ethantran35@hotmail.com>
2025-02-27 20:23:18 -0800

    Fixed Database types

--- backend/models/UserListeningData.js
+++ backend/models/UserListeningData.js
@@ -2,9 +2,17 @@ const mongoose = require("mongoose");
 
 const UserListeningDataSchema = new mongoose.Schema({
     userID: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-    topTracks: {type: [String]},
-    topArtists: {type: [String]},
-    profile: {type: object}
+    topTracks: {type: [String], required: false},
+    topArtists: {type: [String], required: false},
+    profile: {
+        type: {
+            displayName: { type: String }, 
+            profileID: { type: String }, 
+            followers: { type: Number }, 
+            profileURL: { type: String } 
+        },
+        required: false
+    }
 })
 
 const UserListeningData = mongoose.model("UserListeningData", UserListeningDataSchema);
