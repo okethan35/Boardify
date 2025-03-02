6093218304b9b4f81dc0767098020da28a3f512b
okethan35 <ethantran35@hotmail.com>
2025-03-02 14:18:30 -0800

    Updated Database Schema

--- backend/models/UserListeningData.js
+++ backend/models/UserListeningData.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const UserListeningDataSchema = new mongoose.Schema({
     userID: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-    topTracks: {type: [String], required: false},
-    topArtists: {type: [String], required: false},
+    topTracks: [{type: [String], required: false}],
+    topArtists: [{type: [String], required: false}],
     profile: {
         type: {
             displayName: { type: String }, 
             profileID: { type: String }, 
-            followers: { type: Number }, 
+            followers: { type: String }, 
             profileURL: { type: String } 
         },
         required: false
