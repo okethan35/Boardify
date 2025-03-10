caa02a90ff695366aa47cfc0ca7c48a0a57d9d15
dhruvpatel-05 <dhruvp2106@gmail.com>
2025-03-09 19:57:50 -0700

    Fixed QR Code Link Inconsistency

--- backend/routes/qrRoutes.js
+++ backend/routes/qrRoutes.js
@@ -3,6 +3,6 @@ const { searchUsers } = require('../controllers/qrController');  // Import the c
 
 const router = express.Router();
 
-router.get('/generate/${username}', generateQRCode);
+router.get('/:username', generateQRCode);
 
 module.exports = router;
