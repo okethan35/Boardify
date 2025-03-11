e98a3e34203bdb2c262f141a5c7d3c9d1661a724
dhruvpatel-05 <dhruvp2106@gmail.com>
2025-03-11 00:59:02 -0700

    Changing QR Code redirect link

--- backend/controllers/qrController.js
+++ backend/controllers/qrController.js
@@ -1,5 +1,5 @@
 const qrcode = require('qrcode'); // Import the QR code library
-const API_URL = process.env.REACT_APP_API_URL; 
+
 
 // Controller function to generate a QR code for a user's unique link
 exports.generateQRCode = async (req, res) => {
@@ -9,9 +9,9 @@ exports.generateQRCode = async (req, res) => {
     if (!username) {
       return res.status(400).json({ message: 'Username is required' });
     }
-
+    const FRONTEND_URL = req.headers.origin
     // Construct the URL where the QR code should redirect to
-    const url = `${API_URL}/qr/${username}`;
+    const url = `${FRONTEND_URL}/qr/${username}`;
 
     // Generate QR code for the constructed URL
     const qrCode = await qrcode.toDataURL(url); // This will generate the QR code as a base64 PNG image
