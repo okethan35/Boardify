3573da125e5cc2efc59ae6540b83fe4915e4d8cb
okethan35 <ethantran35@hotmail.com>
2025-03-12 20:29:53 -0700

    Update qrController.js

--- backend/controllers/qrController.js
+++ backend/controllers/qrController.js
@@ -4,7 +4,7 @@ const qrcode = require('qrcode'); // Import the QR code library
 exports.generateQRCode = async (req, res) => {
   try {
     const { username } = req.params;
-
+    console.log(username);
     if (!username) {
       return res.status(400).json({ message: 'Username is required' });
     }
@@ -12,7 +12,6 @@ exports.generateQRCode = async (req, res) => {
     // Construct the URL where the QR code should redirect to
     const url = `${FRONTEND_URL}/qr/${username}`;
     const qrCode = await qrcode.toDataURL(url);
-    console.log("QRCODE:", qrCode);
     res.json({ qrCode });
   } catch (error) {
     console.error(error);
